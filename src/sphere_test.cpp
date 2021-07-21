c10e00b7ba5ed3d72a5d586cc5e460af50788bbf
christopher dilks <christopher.j.dilks@gmail.com>
2021-07-21 14:16:13 -0400

    remove builder functions
    This was requested by Whit, to see if it would solve the overlap issue. Initial tests indicate that it does not.

--- src/sphere_test.cpp
+++ src/sphere_test.cpp
@@ -13,83 +13,6 @@ using namespace std;
 using namespace dd4hep;
 using namespace dd4hep::rec;
 
-
-// builders
-namespace sphere_test_builder {
-
-  void build_sphere(Detector &desc, DetElement &sdet, Volume &env, xml::Component plm) {
-
-    // attributes
-    auto mat = desc.material(plm.attr<std::string>(_Unicode(material)));
-    auto vis = desc.visAttributes(plm.attr<std::string>(_Unicode(vis)));
-    double centerx = plm.attr<double>(_Unicode(centerx));
-    double centery = plm.attr<double>(_Unicode(centery));
-    double centerz = plm.attr<double>(_Unicode(centerz));
-    double radius = plm.attr<double>(_Unicode(radius));
-    double thickness = plm.attr<double>(_Unicode(thickness));
-    double thetamin = plm.attr<double>(_Unicode(thetamin));
-    double thetamax = plm.attr<double>(_Unicode(thetamax));
-    double phimin = plm.attr<double>(_Unicode(phimin));
-    double phimax = plm.attr<double>(_Unicode(phimax));
-    double yrot = plm.attr<double>(_Unicode(yrot));
-
-    // optical surface
-    //OpticalSurfaceManager surfMgr = desc.surfaceManager();
-    //auto surf = surfMgr.opticalSurface(dd4hep::getAttrOrDefault(plm, _Unicode(surface), "MirrorOpticalSurface"));
-
-    // volume
-    Volume vol("mirror_v");
-    vol.setMaterial(mat);
-    vol.setVisAttributes(vis);
-    vol.setSolid(Sphere( radius-thickness, radius, thetamin, thetamax, phimin, phimax ));
-
-    // placement
-    auto pv = env.placeVolume(vol,
-      Translation3D(0,0,0) * RotationY(yrot)
-    );
-
-    // apply surface properties
-    DetElement de(sdet, "mirror_de", 0);
-    de.setPlacement(pv);
-    //SkinSurface skin(desc, de, "mirror_optical_surface", surf, vol);
-    //skin.isValid();
-  };
-
-
-  void build_box(Detector &desc, DetElement &sdet, Volume &env, xml::Component plm, SensitiveDetector &sens) {
-
-    // attributes
-    auto mat = desc.material(plm.attr<std::string>(_Unicode(material)));
-    auto vis = desc.visAttributes(plm.attr<std::string>(_Unicode(vis)));
-    double centerx = plm.attr<double>(_Unicode(centerx));
-    double centery = plm.attr<double>(_Unicode(centery));
-    double centerz = plm.attr<double>(_Unicode(centerz));
-    double lx = plm.attr<double>(_Unicode(halflengthx));
-    double ly = plm.attr<double>(_Unicode(halflengthy));
-    double lz = plm.attr<double>(_Unicode(halflengthz));
-
-    // volume
-    Volume vol("sensor_v");
-    vol.setMaterial(mat);
-    vol.setVisAttributes(vis);
-    vol.setSensitiveDetector(sens);
-    vol.setSolid(Box(lx,ly,lz));
-
-    // placement
-    auto pv = env.placeVolume(vol,
-      Translation3D(0,0,0) * RotationX(0)
-    );
-
-    pv.addPhysVolID("sensor", 0);
-    DetElement de(sdet, "sensor_de", 0);
-    de.setPlacement(pv);
-  };
-
-};
-
-// ====================================================
-
-
 // create the detector
 static Ref_t create_detector(Detector& desc, xml::Handle_t handle, SensitiveDetector sens) {
 
@@ -101,19 +24,71 @@ static Ref_t create_detector(Detector& desc, xml::Handle_t handle, SensitiveDete
   xml::Component dims = detElem.dimensions();
 
   // envelope
-  double lx = dims.attr<double>(_Unicode(halflengthx));
-  double ly = dims.attr<double>(_Unicode(halflengthy));
-  double lz = dims.attr<double>(_Unicode(halflengthz));
-  Box envShape(lx,ly,lz);
+  double envx = dims.attr<double>(_Unicode(halflengthx));
+  double envy = dims.attr<double>(_Unicode(halflengthy));
+  double envz = dims.attr<double>(_Unicode(halflengthz));
+  Box envShape(envx,envy,envz);
   auto envMat = desc.material(dd4hep::getAttrOrDefault(detElem, _Unicode(material), "AirOptical"));
   Volume envVol(detName + "_envelope", envShape, envMat);
   envVol.setVisAttributes(desc.visAttributes(detElem.visStr()));
 
-  // children
-  sphere_test_builder::build_sphere(desc, det, envVol, detElem.child(_Unicode(sphere)));
-  sphere_test_builder::build_box(desc, det, envVol, detElem.child(_Unicode(sensor)),sens);
+  // build sphere --------------------------
+  // attributes
+  auto sphereMat = desc.material(detElem.child(_Unicode(sphere)).attr<std::string>(_Unicode(material)));
+  auto sphereVis = desc.visAttributes(detElem.child(_Unicode(sphere)).attr<std::string>(_Unicode(vis)));
+  double sphx = detElem.child(_Unicode(sphere)).attr<double>(_Unicode(centerx));
+  double sphy = detElem.child(_Unicode(sphere)).attr<double>(_Unicode(centery));
+  double sphz = detElem.child(_Unicode(sphere)).attr<double>(_Unicode(centerz));
+  double radius = detElem.child(_Unicode(sphere)).attr<double>(_Unicode(radius));
+  double thickness = detElem.child(_Unicode(sphere)).attr<double>(_Unicode(thickness));
+  double thetamin = detElem.child(_Unicode(sphere)).attr<double>(_Unicode(thetamin));
+  double thetamax = detElem.child(_Unicode(sphere)).attr<double>(_Unicode(thetamax));
+  double phimin = detElem.child(_Unicode(sphere)).attr<double>(_Unicode(phimin));
+  double phimax = detElem.child(_Unicode(sphere)).attr<double>(_Unicode(phimax));
+  double yrot = detElem.child(_Unicode(sphere)).attr<double>(_Unicode(yrot));
 
-  // place mother volume
+  // volume
+  Volume sphereVol("mirror_v");
+  sphereVol.setMaterial(sphereMat);
+  sphereVol.setVisAttributes(sphereVis);
+  sphereVol.setSolid(Sphere( radius-thickness, radius, thetamin, thetamax, phimin, phimax ));
+
+  // placement
+  auto spherePV = envVol.placeVolume(sphereVol,
+    Translation3D(0,0,0) * RotationY(yrot)
+  );
+  DetElement sphereDE(det, "mirror_de", 0);
+  sphereDE.setPlacement(spherePV);
+  
+
+  // build box --------------------------
+  // attributes
+  auto boxMat = desc.material(detElem.child(_Unicode(sensor)).attr<std::string>(_Unicode(material)));
+  auto boxVis = desc.visAttributes(detElem.child(_Unicode(sensor)).attr<std::string>(_Unicode(vis)));
+  double boxx = detElem.child(_Unicode(sensor)).attr<double>(_Unicode(centerx));
+  double boxy = detElem.child(_Unicode(sensor)).attr<double>(_Unicode(centery));
+  double boxz = detElem.child(_Unicode(sensor)).attr<double>(_Unicode(centerz));
+  double lx = detElem.child(_Unicode(sensor)).attr<double>(_Unicode(halflengthx));
+  double ly = detElem.child(_Unicode(sensor)).attr<double>(_Unicode(halflengthy));
+  double lz = detElem.child(_Unicode(sensor)).attr<double>(_Unicode(halflengthz));
+
+  // volume
+  Volume boxVol("sensor_v");
+  boxVol.setMaterial(boxMat);
+  boxVol.setVisAttributes(boxVis);
+  boxVol.setSensitiveDetector(sens);
+  boxVol.setSolid(Box(lx,ly,lz));
+
+  // placement
+  auto boxPV = envVol.placeVolume(boxVol,
+    Translation3D(0,0,0) * RotationX(0)
+  );
+  boxPV.addPhysVolID("sensor", 0);
+  DetElement boxDE(det, "sensor_de", 0);
+  boxDE.setPlacement(boxPV);
+
+
+  // place mother volume ---------
   Volume motherVol = desc.pickMotherVolume(det);
   PlacedVolume envPV = motherVol.placeVolume(envVol, Position(0, 0, 0));
   envPV.addPhysVolID("system", detID);
